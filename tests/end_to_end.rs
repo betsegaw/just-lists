255ff3250e550d11ea1cdc8549ec9d01560b7bd0
Betsegaw Tadele <betsegaw.ta@gmail.com>
2025-12-05 16:59:29 -0800

    cargo fmt

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -8,9 +8,12 @@ fn golden_test() -> Result<(), String> {
         .expect("Required `vhs` command was not accessible.");
 
     assert!(check_vhs.status.success(), "vhs command is not available");
-    
+
     let mut temp_folder = tempfile::tempdir().expect("Unable to create temporary directory.");
-    _ = fs::copy("./docs/demo.tape", format!("{}/demo.tape",temp_folder.path().to_str().unwrap()));
+    _ = fs::copy(
+        "./docs/demo.tape",
+        format!("{}/demo.tape", temp_folder.path().to_str().unwrap()),
+    );
 
     let run_demo_file = Command::new("vhs")
         .arg("demo.tape")
@@ -20,16 +23,21 @@ fn golden_test() -> Result<(), String> {
     match run_demo_file {
         Ok(_) => {
             let old_golden_file = fs::read_to_string("./docs/golden.ascii").unwrap();
-            let new_golden_file = fs::read_to_string(format!("{}/golden.ascii",temp_folder.path().to_str().unwrap())).unwrap();
+            let new_golden_file = fs::read_to_string(format!(
+                "{}/golden.ascii",
+                temp_folder.path().to_str().unwrap()
+            ))
+            .unwrap();
 
             if old_golden_file != new_golden_file {
                 return Err("Output of the golden scenario has changed. Please regenerate using `vhs demo.tape` if the changes are expected".to_string());
             }
 
             Ok(())
-        },
-        Err(e) => {
-            Err(format!("Generating the run execution failed. Code: {}", e.raw_os_error().unwrap()))
         }
+        Err(e) => Err(format!(
+            "Generating the run execution failed. Code: {}",
+            e.raw_os_error().unwrap()
+        )),
     }
 }
